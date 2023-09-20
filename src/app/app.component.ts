56c46b3f5700b58965de4ec7cd0a6365f3577b65
Chris Perko <chris@chrisperko.net>
2023-09-20 17:06:37 -0400

    Reformatting with prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,29 @@
-import {Component} from '@angular/core';
-import {Item} from './models/item';
-import {CartComponent} from './cart/cart.component';
+import { Component } from '@angular/core';
+import { Item } from './models/item';
+import { CartComponent } from './cart/cart.component';
 
 @Component({
-    selector: 'binx-root',
-    standalone: true,
-    imports: [
-        CartComponent
-    ],
-    template: `
-        <binx-cart [items]="items" />
-    `,
-    styles: [`
-    :host {
+  selector: 'binx-root',
+  standalone: true,
+  imports: [CartComponent],
+  template: ` <binx-cart [items]="items" /> `,
+  styles: [
+    `
+      :host {
         display: block;
-        background-image: url("../assets/spooky_night_sky.png");
+        background-image: url('../assets/spooky_night_sky.png');
         background-size: cover;
         height: 100vh;
         padding: 200px 0 0;
-    }
-    `],
+      }
+    `,
+  ],
 })
 export class AppComponent {
-    items: Item[] = [
-        {name: 'Dead Man\'s Toe', price: 24.95, quantity: 3},
-        {name: 'Newt Saliva', price: 9.99, quantity: 1},
-        {name: 'Pox', price: 14.85, quantity: 1},
-        {name: 'Bit of Witche\'s Tongue', price: 59.99, quantity: 1}
-    ];
+  items: Item[] = [
+    { name: "Dead Man's Toe", price: 24.95, quantity: 3 },
+    { name: 'Newt Saliva', price: 9.99, quantity: 1 },
+    { name: 'Pox', price: 14.85, quantity: 1 },
+    { name: "Bit of Witche's Tongue", price: 59.99, quantity: 1 },
+  ];
 }
