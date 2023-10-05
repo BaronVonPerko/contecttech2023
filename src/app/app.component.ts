1659ccb609adca7bb0732953127ea12da0782558
Chris <perko@herodevs.com>
2023-10-04 20:19:01 -0400

    Fixes item name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,6 @@ export class AppComponent {
     { name: "Dead Man's Toe", price: 24.95, quantity: 3 },
     { name: 'Newt Saliva', price: 9.99, quantity: 1 },
     { name: 'Vial of Pox', price: 14.85, quantity: 1 },
-    { name: "Bit of Witch's Tongue", price: 59.99, quantity: 1 },
+    { name: "Witch's Tongue", price: 59.99, quantity: 1 },
   ];
 }
